bde7e53f8c2502e82287b868332b0cc6710d2456
Kyle Schuller <33623211+KyleSchuller@users.noreply.github.com>
2023-07-04 23:27:03 +0100

    Add a couple files to utilities for testing

---
+++ utilities/scripts/debugWithColor.mjs
@@ -0,0 +1,103 @@
+const LOG_COLOR = new Map([
+  [
+    'DARK',
+    {
+      SLATE: '#94a3b8',
+      GRAY: '#9ca3af',
+      ZINC: '#a1a1aa',
+      NEUTRAL: '#a3a3a3',
+      STONE: '#a8a29e',
+      RED: '#f87171',
+      ORANGE: '#fb923c',
+      AMBER: '#fbbf24',
+      YELLOW: '#facc15',
+      LIME: '#a3e635',
+      GREEN: '#4ade80',
+      EMERALD: '#34d399',
+      TEAL: '#2dd4bf',
+      CYAN: '#22d3ee',
+      SKY: '#38bdf8',
+      BLUE: '#60a5fa',
+      INDIGO: '#818cf8',
+      VIOLET: '#a78bfa',
+      PURPLE: '#c084fc',
+      FUSCHIA: '#e879f9',
+      PINK: '#f472b6',
+      ROSE: '#fb7185',
+    },
+  ],
+  [
+    'LIGHT',
+    {
+      SLATE: '#475569',
+      GRAY: '#4b5563',
+      ZINC: '#52525b',
+      NEUTRAL: '#525252',
+      STONE: '#57534e',
+      RED: '#dc2626',
+      ORANGE: '#ea580c',
+      AMBER: '#d97706',
+      YELLOW: '#ca8a04',
+      LIME: '#65a30d',
+      GREEN: '#16a34a',
+      EMERALD: '#059669',
+      TEAL: '#0d9488',
+      CYAN: '#0891b2',
+      SKY: '#0284c7',
+      BLUE: '#2563eb',
+      INDIGO: '#4f46e5',
+      VIOLET: '#7c3aed',
+      PURPLE: '#9333ea',
+      FUSCHIA: '#c026d3',
+      PINK: '#db2777',
+      ROSE: '#e11d48',
+    },
+  ],
+]);
+
+/**
+ * Returns a formatted console message with colors
+ *
+ * @param {string[]} messages - The messages to log.
+ * @param {string[]} colors - The colors to use for each message.
+ * @param {Object} debugConfig - The debug configuration.
+ * @param {boolean} debugConfig.ENABLE - Whether to actually log the messages.
+ * @param {string} debugConfig.THEME - The color theme to use.
+ * @param {string} debugConfig.LOCATION - The location to prepend to each message.
+ * @param {string} debugConfig.SEPARATOR - The separator to append to each message.
+ */
+export default function debugWithColor(messages, colors, { ENABLE = false, THEME = 'DARK', LOCATION = '', SEPARATOR = '⟼' } = {}) {
+  if (!ENABLE) return [];
+
+  if (!Array.isArray(messages) || !Array.isArray(colors)) {
+    console.warn('debugWithColor: Both messages and colors must be arrays.');
+    return [];
+  }
+
+  if (messages.length !== colors.length) {
+    console.warn('debugWithColor: Messages and colors must have the same length.');
+    return [];
+  }
+
+  const colorTheme = LOG_COLOR.get(THEME.toUpperCase());
+  if (!colorTheme) {
+    console.warn(`debugWithColor: Invalid theme "${THEME}".`);
+    return [];
+  }
+
+  const formattedMessages = [`%c${LOCATION}`];
+  const formattedColors = [`color: ${colorTheme['VIOLET']}`];
+
+  messages.forEach((message, index) => {
+    const colorName = colors[index].toUpperCase();
+    const color = colorTheme[colorName];
+    if (!color) {
+      console.warn(`debugWithColor: Invalid color "${colorName}".`);
+      return;
+    }
+    formattedMessages.push(`%c ${SEPARATOR} ${message}`);
+    formattedColors.push(`color: ${color}`);
+  });
+
+  return [formattedMessages.join(''), ...formattedColors];
+}
---
+++ utilities/scripts/docEventPromise.mjs
@@ -0,0 +1,49 @@
+const CONSTANTS = {
+  DEFAULT: {
+    CANCELLABLE: false,
+    EVENT: 'DOMContentLoaded',
+    TIMEOUT: 5000,
+  },
+};
+
+/**
+ * Returns a Promise that resolves when a specific event on the document occurs.
+ *
+ * @param {Object} [options] - The options for the Promise.
+ * @param {string} [options.event='DOMContentLoaded'] - The event to wait for.
+ * @param {number} [options.timeout=5000] - The maximum time to wait for the event, in milliseconds.
+ * @param {boolean} [options.cancellable=false] - Whether the Promise should be cancellable.
+ * @returns {Promise} A Promise that resolves when the event occurs or the timeout expires.
+ */
+export default function docEventPromise({ event = CONSTANTS.DEFAULT.EVENT, timeout = CONSTANTS.DEFAULT.TIMEOUT, cancellable = CONSTANTS.DEFAULT.CANCELLABLE } = {}) {
+  let timer;
+  let listener;
+
+  const promise = new Promise((resolve, reject) => {
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
+      resolve();
+    } else {
+      listener = resolve;
+      document.addEventListener(event, listener, { once: true });
+
+      if (timeout) {
+        timer = setTimeout(() => {
+          document.removeEventListener(event, listener);
+          reject(new Error(`Document not ready after ${timeout}ms`));
+        }, timeout);
+      }
+    }
+  });
+
+  if (cancellable) {
+    return {
+      promise,
+      cancel: () => {
+        clearTimeout(timer);
+        document.removeEventListener(event, listener);
+      },
+    };
+  }
+
+  return promise;
+}
